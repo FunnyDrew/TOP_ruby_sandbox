c2f0cd965dad178426ed0c20114a72b5bb58b798
Rybin Andrey <rybin.ag@gmail.com>
2021-08-05 09:20:32 +0300

    caesar cipher done

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,12 +1,15 @@
 #!/bin/env ruby
-ALPHABET = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+ALPHABET = ("a".."z").to_a
 def cipher(text, shift)
+    return "Wrong shift, must be integer between 0 and 26" unless (-26..26).include? shift
+    real_shift = shift > 0 ? shift-26 : shift    
     result = ''
     text.each_char do |char|
-        newchar = ALPHABET.include?(char.downcase) ?  (char.downcase == char ? ALPHABET[ALPHABET.index(char.downcase)-shift] 
-        : ALPHABET[ALPHABET.index(char.downcase)-shift].upcase)
+        newchar = ALPHABET.include?(char.downcase) ?  
+        (char.downcase == char ? ALPHABET[ALPHABET.index(char.downcase)+real_shift] 
+        : ALPHABET[ALPHABET.index(char.downcase)+real_shift].upcase)
         : char
         result<<newchar
-    end
-    puts result
+    end    
+    result
 end
--- caesar_cipher/spec/caesar_cipher_spec.rb
+++ caesar_cipher/spec/caesar_cipher_spec.rb
@@ -1,13 +1,19 @@
 require_relative "../caesar_cipher"
-describe "caesar cipher testing" 
+describe "caesar cipher testing" do
     it "zero shift" do
         expect(cipher("Hello, Ruby!", 0)).to eq("Hello, Ruby!")
     end
     
-    it "positive shift" do
+    it "negative shift" do
+        expect(cipher("Hello, Ruby!", -3)).to eq("Ebiil, Oryv!")        
     end
 
-    it "negative shift" do
+    it "positive shift" do
+        expect(cipher("Hello, Ruby!", 5)).to eq("Mjqqt, Wzgd!")
+    end
+
+    it "wrong shift" do
+        expect(cipher("Hello, Ruby!", 32)).to eq("Wrong shift, must be integer between 0 and 26");
     end
 
 end
