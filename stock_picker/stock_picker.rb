2d666af1023c7b2a825c93ef24cb724081d69a58
FunnyDrew <rybin.ag@gmail.com>
2021-08-11 07:11:11 +0300

    stock_picker add some

--- stock_picker/stock_picker.rb
+++ stock_picker/stock_picker.rb
@@ -1,6 +1,12 @@
 def stock_picker(prices)
     return "input should be not empty array" if prices.empty?
-    #min_price = prices.take(prices.length-1).min
-    #max_price = prices.drop(1).max
-
+    
+    buy_days = prices.take(prices.length-1)
+    sell_days = prices.drop(1)
+    for day in days
+        
+    end
+    
 end
+
+stock_picker([6, 7, 2, 1, 3, 4, 8, 10, 7, 8]
